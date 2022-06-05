98dd84dd606933eebac09b1e1899ead33f46aa85
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:20:41 +0300

    Update user.js

--- managers/user.js
+++ managers/user.js
@@ -1,6 +1,23 @@
+const businessPage = require('../pages/business/mainPage')
+const getLvlOfRefs = require('../pages/refs/lvlsOfRef')
 const getProfile = require('../pages/profile')
+const mainPageWallet = require('../pages/wallet/mainPage')
+//const accept = require('../pages/wallet/accept')
 const changeQiwi = require('../pages/changeQiwi')
+const infoPage = require('../pages/info')
 const getTops = require('../pages/top')
+const { mainBoard } = require('../keyboards/usual')
+const avatar = require('../pages/avatar')
+const privileges = require("../pages/privilegesPage/privileges")
+const statuses = require("../pages/privilegesPage/statuses")
+const changeNick = require("../pages/privilegesPage/changeNick")
+const getBonus = require("../pages/privilegesPage/getBonus")
+const pets = require("../pages/privilegesPage/pets")
+const myPet = require("../pages/privilegesPage/myPet")
+const managmentPrivilege = require("../pages/privilegesPage/managmentPrivilege")
+const { get } = require('express/lib/request')
+const myPetLevel = require("../pages/privilegesPage/myPetLevel")
+
 module.exports = userManager = async (msg) => {
 
     commands = {
@@ -9,10 +26,26 @@ module.exports = userManager = async (msg) => {
             keyboard: mainBoard(msg.user.admin)
         }),
         profile: () => getProfile(msg),
+        changeNick: () => changeNick(msg),
+        getBonus: () => getBonus(msg),
+        myPet: () => myPet(msg),
+        improvePet: () => myPetLevel(msg),
+        managmentPrivilege: () => managmentPrivilege(msg),
+        /* Бизнес сторона бота */
+        business: () => businessPage(msg),
+
+        /* Реферальная сторона бота */
+        getRefsLvl: () => getLvlOfRefs(msg),
+        wallet: () => mainPageWallet(msg),
         changeQiwi: () => changeQiwi(msg),
+        //accept: () => accept(msg),
+        info: () => infoPage(msg),
         topInvested: () => getTops(msg, 'invested'),
         topRef: () => getTops(msg, 'ref.value'),
-
+        changeAvatar: () => avatar(msg),
+        pets: () => pets(msg),
+        privileges: () => privileges(msg),
+        statuses: () => statuses(msg),
     }
 
     try {
