691c0aec1ac1edd59c2ed32b757207039e1e8995
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:19:53 +0300

    Update admin.js

--- managers/admin.js
+++ managers/admin.js
@@ -2,12 +2,15 @@ const { adminMenu } = require("../keyboards/usual")
 const buyCourse = require("../pages/admin/buyCourse")
 const giveBalance = require("../pages/admin/giveBalance")
 const giveBan = require("../pages/admin/giveBan")
+const giveUnban = require("../pages/admin/giveUnban")
 const hackPercent = require("../pages/admin/hackPercent")
 const mailing = require("../pages/admin/mailing")
 const newBonusePost = require("../pages/admin/newBonusePost")
 const givePrivilege = require("../pages/admin/givePrivilege")
-
-
+const givePet = require("../pages/admin/givePet")
+const giveUnbanWithdrawn = require("../pages/admin/giveUnbanWithdrawn")
+const forRef = require("../pages/admin/forRef")
+const makeUnactive = require("../pages/admin/makeUnactive")
 
 module.exports = adminManager = async (msg) => {
 
@@ -22,7 +25,12 @@ module.exports = adminManager = async (msg) => {
         giveBan: () => giveBan(msg),
         giveBalance: () => giveBalance(msg),
         courseBuy: () => buyCourse(msg),
-        givePrivilege: () => givePrivilege(msg)
+        givePrivilege: () => givePrivilege(msg),
+        givePet: () => givePet(msg),
+        forRef: () => forRef(msg),
+        giveUnban: () => giveUnban(msg),
+        giveUnbanWithdrawn: () => giveUnbanWithdrawn(msg),
+        makeUnactive: () => makeUnactive(msg)
     };
 
     try {
