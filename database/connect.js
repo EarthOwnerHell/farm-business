9db554ea1846d3f8eada1f6ecdd7af286c679fa7
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:16:21 +0300

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const { urlDb } = require('../settings/config.json')
-
+const { SettingsModel } = require('./models')
 const connectDb = () => (
     mongoose.connect(urlDb, (err) => {
 
@@ -8,7 +8,16 @@ const connectDb = () => (
     
         console.log('--> Подключили MongoDB')
     
-    })
+    }),
+    SettingsModel.find().then(res => {
+        if (!res.length) {
+          SettingsModel.insertMany([
+            { name: 'roundDuration', value: 40 },
+            { name: '$', value: 0 },
+      
+          ])
+        }
+      })
 )
 
 module.exports = connectDb
