e3fc8e2d74dbbf1980670761179eb350b82f1577
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:20:21 +0300

    Update eventManager.js

--- managers/eventManager.js
+++ managers/eventManager.js
@@ -1,5 +1,18 @@
+const mainPageRefs = require("../pages/refs/mainPageRefs");
+const setAvatar = require("../pages/setAvatar");
+const withdrawnMoney = require("../pages/wallet/withdrawn");
+const no = require("../pages/wallet/no")
+
+
 module.exports = eventManager = (msg) => {
     const commands = {
+        refs: () => mainPageRefs(msg),
+        withdrawn: () => withdrawnMoney(msg),
+        stella: () => setAvatar(msg, 1),
+        bomb: () => setAvatar(msg, 2),
+        red: () => setAvatar(msg, 3),
+        chack: () => setAvatar(msg, 4),
+        no: () => no(msg)
     }
 
     try {
