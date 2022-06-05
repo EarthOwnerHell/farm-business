8d8f3e703532bce87dd36365d9e12ada3748b21f
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:23:13 +0300

    Create giveUnban.js

---
+++ pages/admin/giveUnban.js
@@ -0,0 +1,20 @@
+const { setUnban } = require('../../database/manager/user')
+const { vk, questionManager, getId } = require('../../settings/vk')
+const { formClick } = require('../../tools')
+
+vk.updates.use(questionManager.middleware)
+
+module.exports = async (msg) => {
+
+    const percent = await msg.question('Вставьте ссылку на профиль')
+
+    const userId = await getId(percent.text)
+
+    if (!userId.id || userId.type !== 'user') return msg.send('Ошибка, проверь вводимые данные')
+
+
+    msg.send(`Успешно разбанили ${formClick(userId.id, 'пользователя')}`)
+    
+    setUnban(userId.id)
+
+}
