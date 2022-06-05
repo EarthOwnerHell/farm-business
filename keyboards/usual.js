40c3863702784e2ce9671e203be924663dd0e463
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:19:00 +0300

    Update usual.js

--- keyboards/usual.js
+++ keyboards/usual.js
@@ -6,6 +6,20 @@ const blue = Keyboard.PRIMARY_COLOR
 const red = Keyboard.NEGATIVE_COLOR
 const grey = Keyboard.SECONDARY_COLOR
 
+const makeButton = (label, command, color = Keyboard.SECONDARY_COLOR) => Keyboard.textButton({
+    label,
+    payload: {
+      command
+    },
+    color
+  })
+
+const makeButtonUrl = (label, url) => Keyboard.urlButton({
+    label,
+    url
+  })
+
+
 const textButton = (label, command = label, color = Keyboard.SECONDARY_COLOR) => (
     Keyboard.textButton({
         label,
@@ -25,18 +39,17 @@ const adminButton = (label, admin = label, color = Keyboard.SECONDARY_COLOR) => 
 const mainBoard = (admin) => (
     Keyboard.keyboard([
         [
-            textButton('👤 Профиль', 'profile', green)  
+            textButton('🐣 Профиль', 'profile', green)  
         ],
         [
-            textButton('💰 Финансы', 'finances', red),
-            (textButton('🌾 Склад', "warehouse", red))
+            textButton('💵 Инвестировать', 'business', red),
+            (textButton('😎 Привилегии', "privileges", Keyboard.POSITIVE_COLOR))
         ],
         [
-            textButton('💸 Магазин', 'market', blue),
+            callBack('🎁 Рефералы', 'refs', blue), textButton('💳 Кошелек', 'wallet', blue),
         ],
         [
-            textButton('📊 Статистика', 'info', green), 
-            textButton('💡 Реферальная система', 'ref', red), 
+            textButton('📊 Статистика', 'info', green)
         ],
         admin ? [
             adminButton('Админка', 'admin', red)
@@ -44,19 +57,53 @@ const mainBoard = (admin) => (
     ])
 )
 
+const gameBoard = Keyboard.keyboard([
+    [makeButton('Банк', 'bank', Keyboard.NEGATIVE_COLOR), makeButton('Баланс', 'balance', Keyboard.NEGATIVE_COLOR)],
+    [makeButton('1-12', '1-12', Keyboard.POSITIVE_COLOR), makeButton('13-24', '13-24', Keyboard.POSITIVE_COLOR), makeButton('25-36', '25-36', Keyboard.POSITIVE_COLOR)],
+    [makeButton('Красное', 'red', Keyboard.NEGATIVE_COLOR), makeButton('Чёрный', 'black', Keyboard.SECONDARY_COLOR)],
+    [makeButton('Чётное', 'even', Keyboard.PRIMARY_COLOR), makeButton('Нечётное', 'odd', Keyboard.PRIMARY_COLOR)],
+    [makeButtonUrl('Пополнить', 'https://vk.com/app6887721_-209099092#donate_10&op_1000')],
+
+  ]
+  )
+
+const exitButton = Keyboard.keyboard([
+    [
+        textButton('Отмена', 'exit', red)
+    ]
+])
+
+
 const adminMenu = Keyboard.keyboard([
     [
-        adminButton('Новый бонус за репост', 'newBonus', blue)
+        adminButton('Новый бонус за репост', 'newBonus', blue),
+        adminButton('Cделать неактивной', 'makeUnactive', blue)
     ],
     [
         adminButton('Рассылка', 'mailing', green)
     ],
     [
-        adminButton('Выдать бан', 'giveBan', green)
+        adminButton('Хак процент', 'hackPercent', blue)
+    ],
+    [
+        adminButton('Выдать бан', 'giveBan', green),
+        adminButton('Выдать разбан', 'giveUnban', green),
+        adminButton('Выдать разбан переводов', 'giveUnbanWithdrawn', green),
     ],
     [
         adminButton('Выдать баланс', 'giveBalance', blue)
     ],
+    [
+        adminButton('Сумма за рефа', 'forRef', red)
+    ],
+    [
+        adminButton('Курс при пополнении', 'courseBuy', green)
+    ],
+     [
+        adminButton('Выдать привилегию', 'givePrivilege', grey),
+        adminButton('Выдать пета', 'givePet', grey)
+    ],
+    
     [
         textButton('Назад', 'back', red)
     ]
@@ -66,4 +113,6 @@ const adminMenu = Keyboard.keyboard([
 module.exports = {
     mainBoard,
     adminMenu,
+    exitButton,
+    gameBoard
 }
