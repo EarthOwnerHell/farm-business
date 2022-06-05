1ad7604836355df93d01711163490a6b80ecfddd
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-06 01:13:17 +0300

    Ggg

--- database/manager/bonuse.js
+++ database/manager/bonuse.js
@@ -4,15 +4,26 @@ const { BonuseModel } = require("../models");
 
 const getBonusePost = (id) => BonuseModel.findOne({ id })
 
-const addBonusePost = (id, sumBonuseRepost) => {
+const addBonusePost = (id, sumBonuseRepost, active) => {
 
     const newBonuseModel = new BonuseModel({
         id,
         sumBonuseRepost,
+        active
     })
 
     newBonuseModel.save().then(console.log)
 
+}
+
+const setUnactive = (id) => {
+    BonuseModel.findOneAndUpdate({
+        id
+    }, {
+        $set: {
+            'active': false
+        }
+    }).then()
 }
 
 const addUserRepost = (id, userId) => (
@@ -31,4 +42,5 @@ module.exports = {
     getBonusePost,
     addBonusePost,
     addUserRepost,
+    setUnactive
 }
