8eebdfcedad6915c2ea924d695cfb15520d2cf02
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2022-06-10 02:04:45 +0300

    Update statuses.js

--- pages/privilegesPage/statuses.js
+++ pages/privilegesPage/statuses.js
@@ -2,5 +2,5 @@ const { privilegesBuyButtons } = require('../../keyboards/inline')
 const { vkMsgForPrivileges } = require("../../settings/vk")
 
 module.exports = async(msg) => {
-return vkMsgForPrivileges(msg.senderId, "", "photo-209099203_457272401", privilegesBuyButtons)
+return vkMsgForPrivileges(msg.senderId, "", "photo-211465984_457239037", privilegesBuyButtons)
 }
